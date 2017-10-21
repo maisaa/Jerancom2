a2f420fba5a2324ccc297b00d0ee88dbc1c3b903
mazenalchalah <mazenalchalah@gmail.com>
2017-10-21 19:02:13 +0300

    add home component

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule}from '@angular/forms';
+import { AppComponent } from './app.component';
+import {HttpModule} from '@angular/http'; 
+import {Http, Headers} from '@angular/http';
+
+ 
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
