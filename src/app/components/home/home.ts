1de0cdae8430acf65fbe97aa5327899e51c4c23e
mazenalchalah <mazenalchalah@gmail.com>
2017-10-22 11:27:13 +0300

    home style

---
+++ src/app/components/home/home.ts
@@ -0,0 +1,16 @@
+import { OnInit,Component } from '@angular/core';
+import{Http,Response,Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+// import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.html',
+  styleUrls: ['./home.scss']
+
+
+})
+export class HomeComponent{
+ 
+}
