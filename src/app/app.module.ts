34c47a97b0bb4ecc4559be46b02fefdaa0b559f2
esraahusamia <esraa.hus93@yahoo.com>
2017-10-26 15:21:18 -1200

    add Maintenance and others component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import { Http, Headers } from '@angular/http';
-import { HomeComponent } from './components/home/home';
-import { signupComponent } from './components/signup/signup.component';
-import { ItemComponent } from './components/item/item.component';
 import { FileSelectDirective } from 'ng2-file-upload';
-// import { FileSelectDirective } from 'ng2-file-upload';
-import { MapComponent } from './components/map/map.components';
-//import{AgmCoreModule} from 'angular2-google-maps/core';
 import { AgmCoreModule } from '@agm/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
+/*****************************************************************************************************/
+import { AppComponent } from './app.component';
+import { ItemComponent } from './components/item/item.component';
+import { signupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home';
+import { MapComponent } from './components/map/map.components';
+import { MaintenanceComponent } from './components/maintenance/maintenance';
+import { OthersComponent } from './components/others/others';
 import { ToolsComponent } from './components/tools/tools'
 import { LoginComponent } from './components/login/login.component';
-
+/*****************************************************************************************************/
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     signupComponent,
     ItemComponent,
-    FileSelectDirective,
     MapComponent,
     LoginComponent,
-    ToolsComponent
-
-
+    ToolsComponent,
+    MaintenanceComponent,
+    OthersComponent,
+    FileSelectDirective
   ],
   imports: [
     BrowserModule,
@@ -38,7 +38,6 @@ import { LoginComponent } from './components/login/login.component';
     ReactiveFormsModule,
     BrowserAnimationsModule,
     AgmCoreModule.forRoot({
-
       apiKey: "AIzaSyAaprvcVNVDbJGy7YTZ3OUVCl4c0KvrhTc",
       libraries: ["places"]
     })
@@ -46,4 +45,5 @@ import { LoginComponent } from './components/login/login.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+/*****************************************************************************************************/
 export class AppModule { }
