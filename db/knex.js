6f25e4e6003f6e49b1969d513db24a375d82192e
maisaa <maisaa.alkhedr@gmail.com>
2017-11-05 20:36:06 +0200

    db name change

--- db/knex.js
+++ db/knex.js
@@ -6,7 +6,21 @@ var knex = require('knex')({
       host : 'https://jerancoma.herokuapp.com',
       user : 'postgres',
       password : '123456',
-      database : 'jer2'
+      database : 'jerancomdb'
     }
    });
 module.exports = knex;
+
+
+// var env = process.env.NODE_ENV || 'development';
+// var config = require('../knexfile')[env];
+// var knex = require('knex')({
+
+//     connection: {
+//       host : 'https://jerancoma.herokuapp.com',
+//       user : 'postgres',
+//       password : '123456',
+//       database : 'jer2'
+//     }
+//    });
+// module.exports = knex;
