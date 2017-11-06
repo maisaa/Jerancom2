8c15435ce0a5f3df29024227a21c965509a765e1
maisaa <maisaa.alkhedr@gmail.com>
2017-11-06 12:00:14 +0200

    bbb

--- migrations/20171105165816_users.js
+++ migrations/20171105165816_users.js
@@ -3,7 +3,7 @@ exports.up = function (knex, Promise) {
     return knex.schema.createTable('users', (table) => {
         table.increments('user_id').primary();
         table.text('username').unique().notNull();
-        tble.string('email');
+        table.string('email');
         table.text('password');
         table.text('phone');
         table.float('longitude');
